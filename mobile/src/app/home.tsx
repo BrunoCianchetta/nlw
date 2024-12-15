d09d97532712c9151e83493638c658441ab862f8
BrunoCianchetta <90101727+BrunoCianchetta@users.noreply.github.com>
2024-12-15 14:06:10 -0300

    primeira versao

---
+++ mobile/src/app/home.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import { View, Text, Alert } from "react-native"
+
+import { api } from "@/services/api"
+
+export default function Home(){
+    const[categories, setCategories] = useState()
+
+
+    async function fetchCategories () {
+        try {
+            const { data } = await api.get("/categories")
+            setCategories(data)
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Categorias", "Não foi possivel carregar as categorias.")
+        }
+    }
+
+
+    useEffect(() => {
+        fetchCategories()
+    },[])
+
+    return( <View style={{ flex: 1}}></View>)
+}
